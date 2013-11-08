691db3891f45c2f41f9b6eea1c6e5daf5ada0bcc
James Sacci <james.sacci@prowebinnovations.com>
2013-11-08 16:16:35 -0500

    finished about page

--- mailer.php
+++ mailer.php
@@ -5,19 +5,19 @@
  */
 
 #YOUR E-MAIL
-define('TO', 'james.sacci@prowebinnovations.com');
+define('TO', 'james@jamessacci.com');
 ##E-MAIL SUBJECT
-define('SUBJECT', 'Contact Form From PWI Website');
+define('SUBJECT', 'Contact Form From James Sacci Portfoliio Website');
 
 
 $message = 'Message from: ' . $_POST['name'] . ' <'.$_POST['email'].'>' . "\n
 Phone Number: " . $_POST['phone'] . "\n
-Message: " . $_POST['comments'];
+Comments: " . $_POST['comments'];
 
 
 if (mail(TO, SUBJECT, $message)){
 	
-    header( "." );
+    header( "Location: http://jamessacci.com/thank-you.html" );
 }else{
 	echo ('Error occurred while sending email');
 }
